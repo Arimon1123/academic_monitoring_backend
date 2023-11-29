752f7797d21d943f99226992884791c9243825f3
ArielMonroy99 <ariel.monroy@ucb.edu.bo>
2023-11-29 18:15:23 -0400

    first commit

---
+++ src/main/java/com/example/backend_academic_monitoring/BackendAcademicMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.example.backend_academic_monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAcademicMonitoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAcademicMonitoringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/backend_academic_monitoring/BackendAcademicMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend_academic_monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendAcademicMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
